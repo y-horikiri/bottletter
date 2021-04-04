a538ce7f347952a2d8191170e46eced40a819052
y-horikiri <62491009+y-horikiri@users.noreply.github.com>
2021-04-04 15:48:22 +0900

    最低限のcssを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def full_title(title = "")
+    app_name = "Bottletter"
+    title.empty? ? app_name
+                 : "#{title} | #{app_name}"
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,25 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Bottletter"
+  end
+
   test "should get home" do
     get root_url
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get policy" do
     get privacy_policy_url
     assert_response :success
+    assert_select "title", "プライバシーポリシー | #{@base_title}"
   end
 
   test "should get terms" do
     get terms_url
     assert_response :success
+    assert_select "title", "利用規約 | #{@base_title}"
   end
 end
