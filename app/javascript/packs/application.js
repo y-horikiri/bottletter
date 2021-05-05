71df60a1c32dbcc15a61df997ff37084b1a70a3f
y-horikiri <62491009+y-horikiri@users.noreply.github.com>
2021-05-05 17:50:44 +0900

    Bootstrap3→5にバージョンアップ(とりあえず)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,15 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import 'bootstrap';
+import '../stylesheets/application';
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+require("bootstrap");
+
+const images = require.context("../images", true);
+const imagePath = name => images(name, true);
 
 Rails.start()
 Turbolinks.start()
